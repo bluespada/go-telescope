bca71d4d00bb52923444a02ce6f3af3e43cf605a
bluespada <pentingmain@gmail.com>
2022-07-15 16:18:07 +0800

    feat : add gitignore for node js

--- cmd/telescoped.go
+++ cmd/telescoped.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import (
+	"github.com/bluespada/go-telescope/server"
+)
 
+func main() {
+	telescope := new(server.TelescopeServer)
+	telescope.RunEngine()
 }
--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"sync"
 )
 
 // initialize telescope server type.
@@ -9,9 +10,27 @@ type TelescopeServer struct{}
 
 // Start Http Server and Socket Server at same time.
 func (t *TelescopeServer) RunEngine() {
+
+	Wg := new(sync.WaitGroup)
+
 	server := fiber.New(fiber.Config{
 		DisableStartupMessage: true,
 	})
 
-	server.Listen(":8080")
+	Wg.Add(2)
+
+	go func() {
+		server.Hooks().OnListen(func() error {
+			println("-- Server has started on port :3000")
+			return nil
+		})
+		Wg.Done()
+	}()
+
+	go func() {
+		server.Listen(":3000")
+		Wg.Done()
+	}()
+
+	Wg.Wait()
 }
